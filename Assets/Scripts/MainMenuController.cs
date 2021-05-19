61433d8fc25a89a2b4cbd6f8b57041aa8cb30dca
JoyStickUi <mrfany2014@gmail.com>
2021-05-19 22:55:13 +0700

    menu2

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{   
+    [SerializeField]
+    GameObject loadingScreen;
+    [SerializeField]
+    Slider loadingBar;
+
+    public void Play(){
+        StartCoroutine(LoadLevel());
+    }
+    public void Exit(){
+        Application.Quit();
+    }
+
+    IEnumerator LoadLevel(){
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone){
+            loadingBar.value = operation.progress;
+            yield return null;
+        }
+    }
+}
